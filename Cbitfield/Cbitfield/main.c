18466c17b1c5014df56e6af5b623c18a3682710e
Henry <nengheng.wu@cygia.com>
2020-05-16 20:54:59 +0800

    Update annotation to code

--- Cbitfield/Cbitfield/main.c
+++ Cbitfield/Cbitfield/main.c
@@ -13,18 +13,26 @@
 typedef unsigned int USI; //仅限于为类型定义符号名称
  
 /* 定义简单的结构 */
-struct
+struct Status1
 {
   USI  widthValidated;
   USI  heightValidated;
 } status1;
  
 /* 定义位域结构 */
-struct
+struct Status2
 {
   unsigned INT widthValidated : 1;
   unsigned INT heightValidated : 1;
 } status2;
+
+struct Status3
+{
+    unsigned INT widthValidated : 16;
+    unsigned INT heightValidated : 16;
+    unsigned INT xValidated : 16;
+} status3;
+
 
 struct
 {
@@ -33,18 +41,22 @@ struct
  
 int main( )
 {
+    //不使用位域的Status1占4（int）*2*8 bit, Status2如果只有一个成员变量 ,只占有2（int）*2*8 bit,若有超过32个成员变量,则占4（int）*2*8 bit
     printf( "Memory size occupied by status1 : %lu\n", sizeof(status1));
     printf( "Memory size occupied by status2 : %lu\n", sizeof(status2));
+    printf( "Memory size occupied by status2 : %lu\n", sizeof(status3));
     
     Age.age = 4;
-    printf( "Sizeof( Age ) : %lu\n", sizeof(Age) );
     printf( "Age.age : %d\n", Age.age );
+    printf( "Sizeof( Age ) : %lu\n", sizeof(Age) );
     
     Age.age = 7;
     printf( "Age.age : %d\n", Age.age );
+    printf( "Sizeof( Age ) : %lu\n", sizeof(Age) );
     
     Age.age = 8; // 二进制表示为 1000 有四位，超出
     printf( "Age.age : %d\n", Age.age );
+    printf( "Sizeof( Age ) : %lu\n", sizeof(Age) );
  
     return 0;
 }
